f15ac43af00ad5673b60db312ab1a506893c3883
saqib iqbal <saqiboiqbal7772@gmail.com>
2023-04-11 21:25:29 +0500

    intial commit

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -1,4 +1,62 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappcord {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+contract Dappcord is ERC721{
+
+    struct Channel{
+        uint256 id;
+        string name;
+        uint256 cost;
+        // address owner;
+    }
+
+    address public owner; //owner of the contract
+    uint256 public channelCount = 0; //number of channels
+    uint256 public totalysupply=0;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can create channels");
+        _;
+    }
+
+    mapping (uint256 => Channel) public channels; //mapping of channel id to channel struct
+
+    mapping(uint256=>mapping(address=>bool)) public hasJoined; //mapping of channel id to mapping of address to bool
+
+    constructor (string memory name, string memory symbol) ERC721(name, symbol) {
+        owner = msg.sender;
+
+    } //constructor
+
+    function createchannel(string memory _name, uint256 _cost) public onlyOwner{
+        channelCount++;
+        channels[channelCount]=Channel (channelCount,_name,_cost);
+        
+    }
+
+    function getChannel (uint256 _id) public view returns (Channel memory){
+    return channels[_id];        
+    }
+
+    function mint(uint256 _id) public payable{
+        require(_id!=0);
+        require(_id<=channelCount);
+        require(hasJoined[_id][msg.sender]==false); //check if user has already joined
+        require(msg.value>=channels[_id].cost); //check if user has paid the cost
+        //join channel
+        hasJoined[_id][msg.sender]=true;
+
+        //Mint NFT 
+        totalysupply++;
+        _safeMint(msg.sender, totalysupply);
+    }
+
+    function withdraw() public onlyOwner{
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+
+}
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,27 @@ const tokens = (n) => {
 }
 
 async function main() {
+  //setup accounts & variables;
+  const [deployer] = await ethers.getSigners()
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
+
+  //deploy contract
+  const Dappcord = await ethers.getContractFactory("Dappcord");
+  const dappcord = await Dappcord.deploy(NAME, SYMBOL);
+  await dappcord.deployed();
+
+  console.log("Dappcord deployed to:", dappcord.address);
+
+  //create channel 3 times
+  const channel_nnsmes = ["General", "intro", "jobs"];
+  const COSTS = [tokens(0.1), tokens(0.2), tokens(0.3)]
+  for (var i = 0; i < 3; i++) {
+    const transcation = await dappcord.connect(deployer).createChannel(channel_nnsmes[i], COSTS[i]);
+    await transcation.wait();
+
+    console.log("Channel created:", channel_nnsmes[i]);
+  }
 
 }
 
